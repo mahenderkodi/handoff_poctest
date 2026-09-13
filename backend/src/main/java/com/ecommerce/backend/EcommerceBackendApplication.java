c555815b5eec6de804df1a24394623977d00b9e5
kodimahender55@gmail.com <mahenderkabali0001@gmail.com>
2026-09-13 15:55:34 +0530

    changed main file

--- backend/src/main/java/com/ecommerce/backend/EcommerceBackendApplication.java
+++ backend/src/main/java/com/ecommerce/backend/EcommerceBackendApplication.java
@@ -3,11 +3,14 @@ package com.ecommerce.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//Demo poc
 @SpringBootApplication
 public class EcommerceBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EcommerceBackendApplication.class, args);
+        System.out.println("Ecommerce Backend Application is running...");
     }
 
 }
